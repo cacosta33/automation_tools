3cdb14777a91fa2deaf2a9197d74b97141a7453a
Cesar Acosta <cesaracosta@cesars-mbp.attlocal.net>
2017-03-21 21:02:43 -0400

    Modified tests

--- tests/integration/components/nav-bar-test.js
+++ tests/integration/components/nav-bar-test.js
@@ -12,23 +12,5 @@ test('it renders', function(assert){
 
   this.render(hbs`{{nav-bar}}`);
 
-  assert.equal(util.trimFiller(this.$().text()), 'Cesar\'s Domain About Tools');
-});
-
-test('can visit about link', function(assert){
-	
-	this.render(hbs`{{nav-bar}}`);
-
-	this.click('#about').then(function(){
-		assert.equal(currentURL(), '/about', 'correct URL was linked to about page');
-	});
-});
-
-test('can visit tools link', function(assert){
-	
-	this.render(hbs`{{nav-bar}}`);
-
-	this.click('#tools').then(function(){
-		assert.equal(currentURL(), '/tools', 'correct URL was linked to tools page');
-	});
+  assert.equal(util.trimFiller(this.$().text()), 'Cesar\'sDomainAboutToolsTool1Tool2');
 });
--- tests/test-utils/utility.js
+++ tests/test-utils/utility.js
@@ -5,7 +5,7 @@ export default Ember.Object.extend({
 	//Remove excess spaces and new line characters
 	trimFiller(str){
 		str = str.trim(); //Remove leading and trailing whitespaces
-		str = str.replace(/(\r\n|\n|\r)/gm, ""); //Remove all new lines
+		str = str.replace(/\s/g, ""); //Remove all new lines
 		str = str.replace(/ +(?= )/g, ""); //Remove all white spaces
 		return str;
 	}
